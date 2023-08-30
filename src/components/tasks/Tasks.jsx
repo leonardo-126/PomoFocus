8a2b9963947d2527514fdaf14e9ee5800e23fcd8
leonardo-126 <leopaiva2016@hotmail.com>
2023-08-30 12:20:31 -0300

    funcionalidade do timer e adcao de funcoes

---
+++ src/components/tasks/Tasks.jsx
@@ -0,0 +1,9 @@
+import './Tasks.scss'
+
+export const Tasks = () => {
+    return (
+        <div className="tasks-container">
+            
+        </div>
+    )
+}
--- src/components/viewPomodoro/ViewPomodoro.jsx
+++ src/components/viewPomodoro/ViewPomodoro.jsx
@@ -4,23 +4,61 @@ import { secondToTime } from './secondToTime/secondToTime'
 
 export const ViewPomodoro = (props) => {
     const [time, setTime] = useState(1500) 
+    const  [start, setStart] = useState(false)
+
+    // navegacao
     useEffect(() => {
         if (props.type == "pomodoro") {
             setTime(1500)
+            setStart(false)
         }else if (props.type == "short") {
             setTime(300)
+            setStart(false)
         }else {
             setTime(3600)
+            setStart(false)
         }
     }, [props.type])
+
+    //timer
+    useEffect(() => {
+        let startTimer
+        if (start && time > 0) {
+            startTimer = setInterval(() => {
+                setTime(prevValor => prevValor - 1) // prevValor pega um valor antigo armazenado no useState
+            }, 1000)
+        } else if (time === 0) {
+            setStart(false)
+            window.alert("Finish your time")
+            if (props.type == "short"){
+                
+            }
+
+
+        }
+        return () => {
+            clearInterval(startTimer)
+        }
+    }, [start, time])
+
+
+    //pausar e iniciar
+    const handleStart = () => {
+        setStart(true)
+    }
+    const handlePause = () => {
+        setStart(false)
+    }
+
+
     return (
         <div className="container-timer">
             <div className="container-timer-timer">
                 <h1>{secondToTime(time)}</h1>
             </div>
             <div className="container-timer-start">
-                <button>
-                    <h1>START</h1>
+                <button onClick={start ? handlePause : handleStart}>
+                    <h1>{start ? 'Pause' : 'Start'}</h1>
                 </button>
             </div>
         </div>
