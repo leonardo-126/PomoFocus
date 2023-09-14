d64b7ce02896b396255886b96116e4cb4cb26559
leonardo-126 <leopaiva2016@hotmail.com>
2023-09-14 14:33:56 -0300

    adicao do st timer

--- src/components/settings/settings.jsx
+++ src/components/settings/settings.jsx
@@ -1,6 +1,6 @@
 import './settings.scss'
 import { AiOutlineClose } from 'react-icons/ai';
-import { PiTimer } from 'react-icons/pi';
+import { BiAlarm } from 'react-icons/bi';
 
 export const Settings = (props) => {
     return (
@@ -15,7 +15,49 @@ export const Settings = (props) => {
                     <i><AiOutlineClose onClick={props.setModalProps}/></i>
                 </div>
                 <div className='container-st-child-content-timer'>
-                    <span><PiTimer/>TIMER</span>
+                    <div className="container-st-child-content-timer-title">
+                        <BiAlarm/><span>TIMER</span>
+                    </div>
+                    <div className="container-st-child-content-timer-stminutes">
+                        <h1>Time (minutes)</h1>
+                        <div className="container-st-child-content-timer-stminutes-setar">
+                            <div className="container-st-child-content-timer-stminutes-setar-pomo">
+                                <p>Pomodoro</p>
+                                <input type="number" value={1}/>
+                            </div>
+                            <div className="container-st-child-content-timer-stminutes-setar-pomo">
+                                <p>Short Break</p>
+                                <input type="number"/>
+                            </div>
+                            <div className="container-st-child-content-timer-stminutes-setar-pomo">
+                                <p>Long Break</p>
+                                <input type="number" value={1} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-st-child-content-timer-breaks">
+                        <h1>Auto Start Breaks</h1>
+                        <div className="container-st-child-content-timer-breaks-setar">
+                            <div className="container-st-child-content-timer-breaks-setar-position">
+                                colocar o button
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-st-child-content-timer-breaks">
+                        <h1>Auto Start Pomodoros</h1>
+                        <div className="container-st-child-content-timer-breaks-setar">
+                            <div className="container-st-child-content-timer-breaks-setar-position">
+                                colocar o button
+                            </div>
+                        </div>
+                    </div>
+                    <div className="container-st-child-content-timer-breaks">
+                        <h1>Long Break interval</h1>
+                        <input type="number" />
+                    </div>
+                </div>
+                <div>
+                    teste
                 </div>
             </div>
         </div>
