fd86434a9b6a1d012de71c8f05e61cf9ab748eb0
leonardo-126 <leopaiva2016@hotmail.com>
2023-09-20 19:16:15 -0300

    adçao do themes no settings sem funcionalidades

--- src/components/SettingsTheme/SettingsTheme.jsx
+++ src/components/SettingsTheme/SettingsTheme.jsx
@@ -1,14 +1,45 @@
 import './SettingsTheme.scss'
-import { BiSolidMagicWand } from 'react-icons/bi';
+import { BiSolidMagicWand, BiSolidDownArrow } from 'react-icons/bi';
+import { IoMdOpen } from 'react-icons/io';
+import { ButtonSettings } from '../buttonSettings/buttonSettings';
 
 export const SettingsTheme = () => {
-    const colorIcon = {
-        color: '#acacac,'
-    }
+
     return (
         <div className="container-theme">
             <div className="container-theme-title">
                 <BiSolidMagicWand/><span>THEME</span>
+            </div>
+            <div className="container-theme-content">
+                <div className="container-theme-content-color">
+                    <h1>Color Themes</h1>
+                    <div className='container-theme-content-color-setar'>
+                        <div className="container-theme-content-color-setar-colors">
+                            
+                        </div>
+                        <div className="container-theme-content-color-setar-colors">
+                            
+                        </div>
+                        <div className="container-theme-content-color-setar-colors">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div className='container-theme-content-color'>
+                    <h1>Hour Format</h1>
+                    <div className='container-theme-content-color-hour'>
+                        <span>24-hour</span>
+                        <BiSolidDownArrow/>
+                    </div>
+                </div>
+                <div className='container-theme-content-color'>
+                    <h1>Dark Mode when running</h1>
+                    <ButtonSettings/>
+                </div>
+                <div className='container-theme-content-color'>
+                    <h1>Small Window</h1>
+                    <button id='button'>Open<IoMdOpen/></button>
+                </div>
             </div>
         </div>
     )
